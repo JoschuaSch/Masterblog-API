1b2fb78eb212a21c8ce4659cc72d26337c146bec
JoschuaSch <schroeder.klosterheide@gmx.de>
2023-07-03 20:57:13 +0200

    Added a Json File and changed some code

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,30 +1,28 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from datetime import datetime
+import json
 
 app = Flask(__name__)
 CORS(app)
 
-POSTS = [
-    {
-        "id": 1,
-        "title": "First post",
-        "content": "This is the first post.",
-        "author": "Author One",
-        "date": "2023-06-07"
-    },
-    {
-        "id": 2,
-        "title": "Second post",
-        "content": "This is the second post.",
-        "author": "Author Two",
-        "date": "2023-06-08"
-    },
-]
+
+def load_posts():
+    try:
+        with open('../posts.json', 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return []
+
+
+def save_posts(posts):
+    with open('../posts.json', 'w') as f:
+        json.dump(posts, f)
 
 
 @app.route('/api/posts', methods=['GET'])
 def get_posts():
+    posts = load_posts()
     sort_key = request.args.get('sort')
     direction = request.args.get('direction')
     valid_sort_keys = ['title', 'content', 'author', 'date']
@@ -36,14 +34,15 @@ def get_posts():
     if sort_key:
         if sort_key == 'date':
             return jsonify(
-                sorted(POSTS, key=lambda x: datetime.strptime(x[sort_key], "%Y-%m-%d"), reverse=direction == 'desc'))
+                sorted(posts, key=lambda x: datetime.strptime(x[sort_key], "%Y-%m-%d"), reverse=direction == 'desc'))
         else:
-            return jsonify(sorted(POSTS, key=lambda x: x[sort_key], reverse=direction == 'desc'))
-    return jsonify(POSTS)
+            return jsonify(sorted(posts, key=lambda x: x[sort_key], reverse=direction == 'desc'))
+    return jsonify(posts)
 
 
 @app.route('/api/posts', methods=['POST'])
 def add_post():
+    posts = load_posts()
     data = request.get_json()
     title = data.get('title')
     content = data.get('content')
@@ -51,7 +50,7 @@ def add_post():
     date = data.get('date')
     if not all([title, content, author, date]):
         return jsonify({"message": "Title, content, author and date are required"}), 400
-    new_id = max(post["id"] for post in POSTS) + 1 if POSTS else 1
+    new_id = max(post["id"] for post in posts) + 1 if posts else 1
     new_post = {
         "id": new_id,
         "title": title,
@@ -59,22 +58,26 @@ def add_post():
         "author": author,
         "date": date,
     }
-    POSTS.append(new_post)
+    posts.append(new_post)
+    save_posts(posts)
     return jsonify(new_post), 201
 
 
 @app.route('/api/posts/<int:post_id>', methods=['DELETE'])
 def delete_post(post_id):
-    post = next((post for post in POSTS if post["id"] == post_id), None)
+    posts = load_posts()
+    post = next((post for post in posts if post["id"] == post_id), None)
     if post is None:
         return jsonify({"message": f"Post with id {post_id} not found"}), 404
-    POSTS.remove(post)
+    posts.remove(post)
+    save_posts(posts)
     return jsonify({"message": f"Post with id {post_id} has been deleted successfully."}), 200
 
 
 @app.route('/api/posts/<int:post_id>', methods=['PUT'])
 def update_post(post_id):
-    post = next((post for post in POSTS if post["id"] == post_id), None)
+    posts = load_posts()
+    post = next((post for post in posts if post["id"] == post_id), None)
     if post is None:
         return jsonify({"message": f"Post with id {post_id} not found"}), 404
     data = request.get_json()
@@ -84,13 +87,15 @@ def update_post(post_id):
         "author": data.get("author", post["author"]),
         "date": data.get("date", post["date"]),
     })
+    save_posts(posts)
     return jsonify(post), 200
 
 
 @app.route('/api/posts/search', methods=['GET'])
 def search_posts():
+    posts = load_posts()
     term = request.args.get('term', '')
-    matching_posts = [post for post in POSTS if
+    matching_posts = [post for post in posts if
                       term in post['title'] or term in post['content'] or term in post['author'] or term in post[
                           'date']]
     return jsonify(matching_posts)
